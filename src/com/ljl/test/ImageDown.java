f118d322e75fdc17218af2c8d2c0cf3364ac870b
ljl <769151928@qq.com>
2022-06-19 00:07:10 +0800

    11

--- src/com/ljl/test/ImageDown.java
+++ src/com/ljl/test/ImageDown.java
@@ -3,6 +3,10 @@ package com.ljl.test;
 public class ImageDown {
     public static void main(String[] args) {
         System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
     }
 
 
