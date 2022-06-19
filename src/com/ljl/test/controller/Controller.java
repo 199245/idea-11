1bf5e851fd7f9c90623a6f2af99c5755ef1d4683
ljl <769151928@qq.com>
2022-06-19 11:52:38 +0800

    全部提交

--- src/com/ljl/test/controller/Controller.java
+++ src/com/ljl/test/controller/Controller.java
@@ -4,6 +4,13 @@ public class Controller {
     //
     public static void main(String[] args) {
         System.out.println("controller");
+        System.out.println("controller");
+        System.out.println("controller");
+        System.out.println("controller");
+        System.out.println("controller");
+        System.out.println("controller");
+        System.out.println("controller");
+        System.out.println("controller");
     }
 }
 
